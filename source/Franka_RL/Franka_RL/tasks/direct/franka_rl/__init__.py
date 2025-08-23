7daa45cb557e5c071eae2e0226728f7e47194953
Dongshen Yang <yyyyds139@gmail.com>
2025-08-23 22:58:11 +0800

    Initial commit

---
+++ source/Franka_RL/Franka_RL/tasks/direct/franka_rl/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Franka-Rl-Direct-v0",
+    entry_point=f"{__name__}.franka_rl_env:FrankaRlEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.franka_rl_env_cfg:FrankaRlEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
